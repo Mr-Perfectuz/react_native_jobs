aaf287d84ace7303ef7313899fdb7c9e17af5b71
Anvar <ziyodovanvar1999@gmail.com>
2025-02-03 23:51:12 +0900

    Feat: develop PopularJobs component

---
+++ hook/useFetch.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts');
+  });
+};
