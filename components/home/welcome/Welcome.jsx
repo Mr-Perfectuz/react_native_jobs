4baae509bb52cd4a0a2010673fe480f1fd215a51
Anvar <anvar.ziyodov@trescal.com>
2025-02-04 10:29:51 +0900

    Feat: develop useFetch hook

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,17 +6,17 @@ import {
   TouchableOpacity,
   Image,
   FlatList,
-} from 'react-native';
-import { useRouter } from 'expo-router';
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style';
-import { icons, SIZES } from '../../../constants';
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
-const jobTypes = ['full-time', 'part-time', 'freelance'];
+const jobTypes = ["full-time", "part-time", "freelance"];
 
 const Welcome = () => {
   const router = useRouter();
-  const [activeJobType, setActiveJobType] = useState('full-time');
+  const [activeJobType, setActiveJobType] = useState("full-time");
   return (
     <View>
       <View style={styles.container}>
@@ -28,15 +28,15 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=''
+            value=""
             onChange={() => {}}
-            placeholder='What are you looking for?'
+            placeholder="What are you looking for?"
           />
         </View>
         <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
           <Image
             source={icons.search}
-            resizeMode='contain'
+            resizeMode="contain"
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,11 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { RAPID_API_KEY } from "@env";
+const rapidApiKey = RAPID_API_KEY;
 
-const useFetch = () => {
+const useFetch = (endpont, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/search/${endpont}`,
+    headers: {
+      "x-rapidapi-key": rapidApiKey,
+      "x-rapidapi-host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts');
-  });
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
 };
